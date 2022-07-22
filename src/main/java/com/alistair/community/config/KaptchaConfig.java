47688f4b5eceb8b92b1cb373702d8051e5d8e7e5
Alistair <1518016264@qq.com>
2022-07-22 22:58:49 +0800

    生成验证码

---
+++ src/main/java/com/alistair/community/config/KaptchaConfig.java
@@ -0,0 +1,34 @@
+package com.alistair.community.config;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class KaptchaConfig {
+
+    @Bean
+    public Producer kaptchaProducer(){
+
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+
+        //通过properties对象装载配置信息,以键值对的方式
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width", "100");
+        properties.setProperty("kaptcha.image.height", "40");
+        properties.setProperty("kaptcha.textproducer.font.size", "32");
+        properties.setProperty("kaptcha.textproducer.font.color", "0,0,0");
+        properties.setProperty("kaptcha.textproducer.char.string", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
+        //所有的第三方导入Bean都是通过config去配置，而config需要依赖于Properties
+        Config config = new Config(properties);
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+
+}
--- src/main/java/com/alistair/community/controller/LoginController.java
+++ src/main/java/com/alistair/community/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.alistair.community.controller;
 import com.alistair.community.entity.User;
 import com.alistair.community.service.UserService;
 import com.alistair.community.util.CommunityConstant;
+import com.google.code.kaptcha.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +13,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
+
+    //日志对象
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private UserService userService;
@@ -67,6 +79,32 @@ public class LoginController implements CommunityConstant {
     @RequestMapping(path = "/login", method = RequestMethod.GET)
     public String getLoginPage(){
         return "/site/login";
+    }
+
+    //验证码的Bean
+    @Autowired
+    private Producer kaptchaProducer;
+
+
+
+
+    //生成验证码
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        //生成验证码(字符串)
+        String text = kaptchaProducer.createText();
+        //生成图片
+        BufferedImage image = kaptchaProducer.createImage(text);
+        //将验证码存入session
+        session.setAttribute("kaptcha", text);
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image,"png", os);
+        } catch (IOException e) {
+            logger.error("响应验证码失败"+e.getMessage());
+        }
     }
 
 }
