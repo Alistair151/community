6e50bbe78266d3310f06a63c2917e2eceec3a0a5
Alistair <1518016264@qq.com>
2022-07-30 00:32:35 +0800

    显示评论

--- src/main/java/com/alistair/community/controller/DiscussPostController.java
+++ src/main/java/com/alistair/community/controller/DiscussPostController.java
@@ -1,10 +1,14 @@
 package com.alistair.community.controller;
 
 import com.alistair.community.annotation.LoginRequired;
+import com.alistair.community.entity.Comment;
 import com.alistair.community.entity.DiscussPost;
+import com.alistair.community.entity.Page;
 import com.alistair.community.entity.User;
+import com.alistair.community.service.CommentService;
 import com.alistair.community.service.DiscussPostService;
 import com.alistair.community.service.UserService;
+import com.alistair.community.util.CommunityConstant;
 import com.alistair.community.util.CommunityUtil;
 import com.alistair.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.jws.WebParam;
-import java.util.Date;
+import java.util.*;
 
 @Controller
 @RequestMapping("/discuss")
-public class DiscussPostController {
+public class DiscussPostController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
@@ -30,6 +33,9 @@ public class DiscussPostController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -52,7 +58,8 @@ public class DiscussPostController {
 
     //查询帖子详情
     @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
-    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page) {
+        //对与page，实体类型的参数，会自动装载到model中，不需要额外addAttribute
         // 帖子
         DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
         model.addAttribute("post", discussPost);
@@ -60,7 +67,55 @@ public class DiscussPostController {
         User user = userService.findUserById(discussPost.getUserId());
         model.addAttribute("user", user);
 
+        //评论分页信息
+        page.setLimit(5);
+        page.setPath("/discuss/detail/" + discussPostId);
+        page.setRows(discussPost.getCommentCount());
+
+        //查询出的对帖子的评论
+        List<Comment> commentList =  commentService.findCommentsByEntity(
+                ENTITY_TYPE_POST,discussPostId, page.getOffset(),page.getLimit());
+        //显示的对象
+        List<Map<String, Object>>  commentViewList = new ArrayList<>();
+        if(commentList != null){
+            for(Comment comment : commentList){
+                //某一个评论
+                Map<String, Object> commentView = new HashMap<>();
+                //评论放进入
+                commentView.put("comment", comment);
+                //作者放进去
+                commentView.put("user", userService.findUserById(comment.getUserId()));
+                //回复列表
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(),0,Integer.MAX_VALUE);
+                //回复的Vo列表
+                List<Map<String, Object>> replyViewList = new ArrayList<>();
+                if (replyList != null) {
+                    for(Comment reply : replyList) {
+                        //某一个回复
+                        Map<String, Object> replyView = new HashMap<>();
+                        //将回复放进入
+                        replyView.put("reply", reply);
+                        //将作者放进去
+                        replyView.put("user", userService.findUserById(reply.getUserId()));
+                        //将回复目标放进去
+                        replyView.put("target", reply.getTargetId()==0?null:userService.findUserById(reply.getTargetId()));
+                        replyViewList.add(replyView);
+                    }
+                }
+                //将这条评论的所有回复放进去
+                commentView.put("replies", replyViewList);
+                //回复的数量
+                int replyCount = commentService.findCountByEntity(ENTITY_TYPE_COMMENT, comment.getId());
+                commentView.put("replyCount", replyCount);
+                commentViewList.add(commentView);
+            }
+        }
+        //将需要显示的评论的所有数据加入到model中
+        model.addAttribute("comments", commentViewList);
         return "/site/discuss-detail";
     }
+
+
 
 }
---
+++ src/main/java/com/alistair/community/dao/CommentMapper.java
@@ -0,0 +1,16 @@
+package com.alistair.community.dao;
+
+import com.alistair.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType, int entityId);
+
+
+}
